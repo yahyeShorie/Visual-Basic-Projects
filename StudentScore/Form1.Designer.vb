d77e948353ebaa26dc00815f437146157d9c06f5
unknown <yahshor9@gmail.com>
2020-10-14 23:28:16 +0300

    added VB.net files

---
+++ StudentScore/Form1.Designer.vb
@@ -0,0 +1,227 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtID = New System.Windows.Forms.TextBox()
+        Me.txtMarks = New System.Windows.Forms.TextBox()
+        Me.txtSubjects = New System.Windows.Forms.TextBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnCal = New System.Windows.Forms.Button()
+        Me.txtAvg = New System.Windows.Forms.TextBox()
+        Me.txtTotal = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Display = New System.Windows.Forms.ListView()
+        Me.ID = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Subject = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.Score = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(14, 21)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(28, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "ID"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(14, 59)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(70, 20)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "Subject"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.Location = New System.Drawing.Point(14, 99)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 20)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Marks"
+        '
+        'txtID
+        '
+        Me.txtID.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtID.Location = New System.Drawing.Point(146, 17)
+        Me.txtID.Multiline = True
+        Me.txtID.Name = "txtID"
+        Me.txtID.Size = New System.Drawing.Size(166, 24)
+        Me.txtID.TabIndex = 1
+        '
+        'txtMarks
+        '
+        Me.txtMarks.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtMarks.Location = New System.Drawing.Point(146, 95)
+        Me.txtMarks.Multiline = True
+        Me.txtMarks.Name = "txtMarks"
+        Me.txtMarks.Size = New System.Drawing.Size(166, 24)
+        Me.txtMarks.TabIndex = 2
+        '
+        'txtSubjects
+        '
+        Me.txtSubjects.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtSubjects.Location = New System.Drawing.Point(146, 55)
+        Me.txtSubjects.Multiline = True
+        Me.txtSubjects.Name = "txtSubjects"
+        Me.txtSubjects.Size = New System.Drawing.Size(166, 24)
+        Me.txtSubjects.TabIndex = 3
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnAdd.Location = New System.Drawing.Point(125, 141)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(67, 31)
+        Me.btnAdd.TabIndex = 4
+        Me.btnAdd.Text = "Add"
+        Me.btnAdd.UseVisualStyleBackColor = True
+        '
+        'btnCal
+        '
+        Me.btnCal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnCal.Location = New System.Drawing.Point(216, 141)
+        Me.btnCal.Name = "btnCal"
+        Me.btnCal.Size = New System.Drawing.Size(96, 31)
+        Me.btnCal.TabIndex = 4
+        Me.btnCal.Text = "Calculate"
+        Me.btnCal.UseVisualStyleBackColor = True
+        '
+        'txtAvg
+        '
+        Me.txtAvg.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtAvg.Location = New System.Drawing.Point(74, 403)
+        Me.txtAvg.Multiline = True
+        Me.txtAvg.Name = "txtAvg"
+        Me.txtAvg.Size = New System.Drawing.Size(76, 36)
+        Me.txtAvg.TabIndex = 7
+        '
+        'txtTotal
+        '
+        Me.txtTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtTotal.Location = New System.Drawing.Point(270, 403)
+        Me.txtTotal.Multiline = True
+        Me.txtTotal.Name = "txtTotal"
+        Me.txtTotal.Size = New System.Drawing.Size(76, 36)
+        Me.txtTotal.TabIndex = 8
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.Location = New System.Drawing.Point(8, 419)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(39, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Avg"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(192, 419)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(49, 20)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Total"
+        '
+        'Display
+        '
+        Me.Display.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ID, Me.Subject, Me.Score})
+        Me.Display.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Display.GridLines = True
+        Me.Display.HideSelection = False
+        Me.Display.Location = New System.Drawing.Point(10, 178)
+        Me.Display.Name = "Display"
+        Me.Display.Size = New System.Drawing.Size(469, 204)
+        Me.Display.TabIndex = 10
+        Me.Display.UseCompatibleStateImageBehavior = False
+        Me.Display.View = System.Windows.Forms.View.Details
+        '
+        'ID
+        '
+        Me.ID.Text = "ID"
+        Me.ID.Width = 144
+        '
+        'Subject
+        '
+        Me.Subject.Text = "Subject"
+        Me.Subject.Width = 139
+        '
+        'Score
+        '
+        Me.Score.Text = "Score"
+        Me.Score.Width = 158
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(482, 451)
+        Me.Controls.Add(Me.Display)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtTotal)
+        Me.Controls.Add(Me.txtAvg)
+        Me.Controls.Add(Me.btnCal)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.txtSubjects)
+        Me.Controls.Add(Me.txtMarks)
+        Me.Controls.Add(Me.txtID)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtID As TextBox
+    Friend WithEvents txtMarks As TextBox
+    Friend WithEvents txtSubjects As TextBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnCal As Button
+    Friend WithEvents txtAvg As TextBox
+    Friend WithEvents txtTotal As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Display As ListView
+    Friend WithEvents ID As ColumnHeader
+    Friend WithEvents Subject As ColumnHeader
+    Friend WithEvents Score As ColumnHeader
+End Class
---
+++ StudentScore/Form1.vb
@@ -0,0 +1,30 @@
+﻿Public Class Form1
+    'Dim Allscore(0) As Integer
+    Dim Total As Integer = 0
+    Dim count As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        Dim Studentdata(3) As String
+        Dim data As ListViewItem
+
+        Dim Id As String = txtID.Text
+        Dim Subject As String = txtSubjects.Text
+        Dim Scores As String = txtMarks.Text
+        Studentdata(0) = Id
+        Studentdata(1) = Subject
+        Studentdata(2) = Scores
+        Total += CInt(Scores)
+        count += 1
+
+        data = New ListViewItem(Studentdata)
+        Display.Items.Add(data)
+
+
+    End Sub
+
+    Private Sub btnCal_Click(sender As Object, e As EventArgs) Handles btnCal.Click
+
+        txtTotal.Text = Total
+        txtAvg.Text = Total / count
+    End Sub
+
+End Class
